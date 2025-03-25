35c6c0ac522cc76683a5ac3520fbc795cb54c713
Benjamin Peyraga <benjapey99@gmail.com>
2025-03-24 21:15:04 -0300

    Auto commit

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
         navList.classList.toggle("active");
     });
 });
+
+let currentIndex = 0;
+
+function changeImage(direction) {
+    const slides = document.querySelectorAll('.slide');
+    currentIndex += direction;
+
+    // Verifica que no se pase de los límites de las imágenes
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+
+    // Mueve las imágenes
+    const offset = -currentIndex * 100;
+    document.querySelector('.image-slider').style.transform = `translateX(${offset}%)`;
+}
