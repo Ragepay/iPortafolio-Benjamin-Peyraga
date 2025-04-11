09f553fc83f111afa850603050aae5bc63dd2cc6
Benjamin Peyraga <benjapey99@gmail.com>
2025-04-11 15:07:47 -0300

    Auto commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector("#mobile-menu");
     const navList = document.querySelector(".nav-list");
 
-    menuToggle.addEventListener("click", function() {
+    menuToggle.addEventListener("click", function () {
         navList.classList.toggle("active");
     });
 });
@@ -23,4 +23,20 @@ function changeImage(direction) {
     // Mueve las imágenes
     const offset = -currentIndex * 100;
     document.querySelector('.image-slider').style.transform = `translateX(${offset}%)`;
+}
+
+function descargarArchivos() {
+    const links = [
+        './docs/Benjamin-Peyraga-CV.pdf',
+        './docs/Benjamin Peyraga CV.pdf'
+    ];
+
+    links.forEach(link => {
+        const a = document.createElement('a');
+        a.href = link;
+        a.download = link.split('/').pop();
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    });
 }
